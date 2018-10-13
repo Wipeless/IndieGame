3140c5a4a783b03163a6d039b2f9f195259e26ac
sslampa <sslampa@gmail.com>
2018-10-12 19:47:55 -0700

    Add initial EnemyBuildingObject
    Add properties for EnemyBuildingObject
    
    Add gun to EnemyBuildingObject

---
+++ Assets/Code/Gameplay/EnemyBuildingObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBuildingObject : GameplayObject {
+
+    public EnemyGunScript m_EnemyGun;
+
+	// Use this for initialization
+	void Start ()
+    {
+        HandleBirth();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (collision.gameObject.tag == "BulletTag")
+        {
+            m_health -= collision.gameObject.GetComponent<BulletObject>().m_BulletStrength;
+
+            if (m_health <= 0)
+            {
+                HandleDeath();
+            }
+        }
+    }
+
+    protected override void HandleBirth()
+    {
+        m_health = 3000;
+        base.HandleBirth();
+    }
+
+    protected override void HandleDeath()
+    {
+        if (!m_isDying)
+        {
+            m_isDying = true;
+            Destroy(this.gameObject);
+            ScoreManager.AddScore(300);
+        }
+    }
+}
